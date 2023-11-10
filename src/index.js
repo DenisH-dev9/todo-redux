1992e358162d5f6ba7e08bcea9b887d38439541c
Denis <0676899640den@gmail.com>
2023-11-10 12:55:30 +0200

    fix app index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line react/no-deprecated
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
